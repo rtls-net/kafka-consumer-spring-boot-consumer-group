6236d6acd6f50700ae4c95a8bee5533d0e4f0032
udit <uditkumar063@gmail.com>
2023-10-28 23:03:59 +0530

    Kafka consumer with multiple kafka consumer group

---
+++ src/main/java/com/eapps/KafkaConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.eapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerExampleApplication.class, args);
+	}
+
+}
